14486790d6322e0ef56e4fddafa1c0bd17416f1d
djauharnehru34-hue <djauharnehru34@gmail.com>
2025-12-03 13:38:29 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-panorama-2-360-8k",
+      "name": "Panorama 2 360 8k",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.2660503966928047,
+        "pitch": 0.06230269828721369,
+        "fov": 0.7778812297069148
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.029381805874109546,
+          "pitch": 0.2694242498816415,
+          "rotation": 0,
+          "target": "0-panorama-2-360-8k"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
